6632c4a7672c99504c43241a617b7d4a577ac824
Eric <campagnollo2002@gmail.com>
2020-07-14 21:37:29 -0400

    Update desec_config.py

--- desec_config.py
+++ desec_config.py
@@ -1,36 +1,46 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter.filedialog import asksaveasfile
 import platform
 import re
 
 def main():
 
     root = Tk()
+
     opsys=platform.system()
     if opsys=='Windows':
-        root.filename = filedialog.askopenfilename(initialdir="C:\\")
+        root.filename = filedialog.askopenfilename(initialdir="C:\\Users\ericmoo\Downloads")
     else:
         root.filename = filedialog.askopenfilename(initialdir="/home",title="Select a linux file")
     file=open(root.filename, "r")
-    desec(file, opsys)
+    dfile = desec(file, opsys)
 
-
-    filename=root.filename
     root.destroy()
     root.mainloop()
 
+    root2 = Tk()
+    f = asksaveasfile(mode='w', defaultextension='.txt')
+    if f is None:
+        exit()
+    root2.destroy()
+    root2.mainloop()
+    for item in dfile:
+        f.write(item)
+    f.close()
+
 
 def desec(file, opsys):
-    if opsys=="Windows":
-        fileloc="C:\\user\ericmoo\desec.txt"
+    """if opsys=="Windows":
+        fileloc="C:\\Users\ericmoo\Downloads\desec.txt"
     else:
-        fileloc="/home/campagnollo/tech/desec.txt"
+        fileloc="/home/campagnollo/tech/desec.txt" """
     removed=re.compile('(<removed>)')
     aaa=re.compile('^[a]{3,3} ')
     endstr=re.compile('^end')
     curconf=re.compile("^Current configuration.*")
     lined=file.readlines()
-    filedesec=open(fileloc,"w")
+    filedesec=[]
     preview=True
     for i in lined:
         if preview==True:
@@ -42,15 +52,15 @@ def desec(file, opsys):
 
 
         if re.search(removed,i) or re.search(aaa,i):
-            filedesec.write("!"+i)
+            filedesec.append("!"+i)
         else:
-            filedesec.write(i)
+            filedesec.append(i)
         if re.search(endstr,i):
-            filedesec.write("\n")
+            filedesec.append("\n")
             break
-    filedesec.close()
-    #file.close()
-    return
+
+
+    return filedesec
 
 
 
