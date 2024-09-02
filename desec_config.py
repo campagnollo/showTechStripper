289d46b5be23167768296becc0ec4f27ecb2678b
Eric <campagnollo2002@gmail.com>
2024-09-02 09:04:25 -0400

    Update desec_config.py
    added docstring

--- desec_config.py
+++ desec_config.py
@@ -5,6 +5,11 @@ import platform
 import re
 
 def main():
+    """
+    This is the main function that handles the file dialog operations and calls the desec function.
+    It first opens a file dialog to select a file, then it reads the file and passes it to the desec function.
+    After the desec function returns, it opens another file dialog to save the processed data into a new file.
+    """
 
     root = Tk()
 
@@ -31,10 +36,12 @@ def main():
 
 
 def desec(file, opsys):
-    """if opsys=="Windows":
-        fileloc="C:\\Users\ericmoo\Downloads\desec.txt"
-    else:
-        fileloc="/home/campagnollo/tech/desec.txt" """
+    """
+    This function takes a file and the operating system as input.
+    It reads the file line by line and processes each line based on certain conditions.
+    If a line matches certain patterns, it is modified and added to the filedesec list.
+    The function returns the filedesec list which contains the processed lines.
+    """
     removed=re.compile('(<removed>)')
     aaa=re.compile('^[a]{3,3} ')
     endstr=re.compile('^end')
@@ -68,4 +75,3 @@ if __name__ == '__main__':
     try: main()
     except UnboundLocalError:
         print("Program cancled")
-
